1b2b0daec22507fc191f9453093dcf5b8385e170
Asmarizwan <55379731+Asmarizwan@users.noreply.github.com>
2020-03-31 18:23:21 +0200

    Search

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, {useContext, useEffect, useState } from 'react';
 import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
 import Login from './Login';
 import AuctionForm  from '../Components/AuctionForm';
 
+import {AuctionContext} from '../contexts/auctionContext';
+
 const NavBar = () => {
+    const appContext = useContext(AuctionContext)
+  const {handleSubmit, handleSearchChange, auctions, handleReturnHome } = appContext
+
     return (
         <Navbar bg="light" variant="light">
             <Navbar.Brand href="#home">Grupp4's auktions sidas namn</Navbar.Brand>
-            <Form inline>
-                <FormControl type="text" placeholder="Söktext" className="mr-sm-2" />
-                <Button variant="outline-primary">Sök</Button>
+            <Form inline onSubmit={(e) => handleSubmit(e)}>
+                <FormControl onChange={(e) => handleSearchChange(e)} type="text" placeholder="Söktext" className="mr-sm-2" />
+                <Button variant="outline-primary" type="button"
+          onClick={() => handleReturnHome()}>Home</Button>
             </Form>
+            <Login/>
             <AuctionForm/>
-            <Login />
         </Navbar>
     )
 }
 
 export default NavBar;
+
+
+
